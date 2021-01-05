579d55aecaa9aae5c97cfc076f8accd77f02ae86
Mohamed Fawzy <mohamed.f.elmorsi@gmail.com>
2021-01-05 17:52:17 +0200

    update map settimeout

--- src/components/Map.js
+++ src/components/Map.js
@@ -12,58 +12,51 @@ const Map = ({ data }) => {
     selectedCountry,
     casesType,
   } = data;
-  const [mounted, setMounted] = useState(false);
   const ChangeView = ({ center, zoom }) => {
     const map = useMap();
     map.setView([center[0] - 5, center[1] - 5], zoom);
     return null;
   };
   useLayoutEffect(() => {
-    setTimeout(() => {
-      setMounted(true);
-    }, 500);
     return () => {
       setMounted(false);
     };
   }, []);
   return (
     <div className="map">
-      {mounted && (
-        <MapContainer center={[selectedLat, selectedLong]} zoom={zoom}>
-          <ChangeView center={[selectedLat, selectedLong]} zoom={zoom} />
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[selectedLat, selectedLong]}>
-            {selectedCountry.countryInfo && (
-              <Popup>
-                <div className="info-container">
-                  <div
-                    className="info-flag"
-                    style={{
-                      backgroundImage: `url(${selectedCountry.countryInfo.flag})`,
-                    }}
-                  ></div>
-                  <div className="info-name">{selectedCountry.country}</div>
-                  <div className="info-confirmed">
-                    Cases: {numeral(selectedCountry.cases).format("0,0")}
-                  </div>
-                  <div className="info-recovered">
-                    Recovered:{" "}
-                    {numeral(selectedCountry.recovered).format("0,0")}
-                  </div>
-                  <div className="info-deaths">
-                    Deaths: {numeral(selectedCountry.deaths).format("0,0")}
-                  </div>
+      <MapContainer center={[selectedLat, selectedLong]} zoom={zoom}>
+        <ChangeView center={[selectedLat, selectedLong]} zoom={zoom} />
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[selectedLat, selectedLong]}>
+          {selectedCountry.countryInfo && (
+            <Popup>
+              <div className="info-container">
+                <div
+                  className="info-flag"
+                  style={{
+                    backgroundImage: `url(${selectedCountry.countryInfo.flag})`,
+                  }}
+                ></div>
+                <div className="info-name">{selectedCountry.country}</div>
+                <div className="info-confirmed">
+                  Cases: {numeral(selectedCountry.cases).format("0,0")}
                 </div>
-              </Popup>
-            )}
-          </Marker>
-          {countries &&
-            showDataOnMap(countries, selectedLat, selectedLong, casesType)}
-        </MapContainer>
-      )}
+                <div className="info-recovered">
+                  Recovered: {numeral(selectedCountry.recovered).format("0,0")}
+                </div>
+                <div className="info-deaths">
+                  Deaths: {numeral(selectedCountry.deaths).format("0,0")}
+                </div>
+              </div>
+            </Popup>
+          )}
+        </Marker>
+        {countries &&
+          showDataOnMap(countries, selectedLat, selectedLong, casesType)}
+      </MapContainer>
     </div>
   );
 };
